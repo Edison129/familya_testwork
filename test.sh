c3eb28c769df4b5d196d84ade6b59037574c9eb6
root <root@DESKTOP-8VAOU7E.>
2025-01-14 11:18:41 +0300

    first commit

---
+++ test.sh
@@ -0,0 +1,8 @@
+##copyright ©Sklyarov Eduard @я обязательно стану девопсом2025
+#!/bin/bash
+declare -i a
+declare -i d;declare -i b
+a=1668;d=1668;b=$a+$d
+echo $b #Выведет 3336
+b="Good" #Попытка присваивания строкового значения
+echo $b
